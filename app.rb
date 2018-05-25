458138924ddd4c247fafd676550e366a0f1465ef
Emily Jirles <emily.jirles@gmail.com>
2018-05-25 17:09:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,6 +18,11 @@ class App < Sinatra::Base
     @number.times do 
       "#{params[:phrase]}"
     end 
+    
+  end
+  
+  get "/say/:word1/:word2/:word3/:word4/:word5" do 
+    " #{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
   end 
 
 end
